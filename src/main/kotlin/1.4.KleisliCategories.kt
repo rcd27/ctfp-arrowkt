eadf8231f73e4f17afb99e1e0a2129817e6d55c9
Stanislav Zemlyakov <redtom@yandex.ru>
2020-09-12 09:32:13 +0300

    add value

--- src/main/kotlin/1.4.KleisliCategories.kt
+++ src/main/kotlin/1.4.KleisliCategories.kt
@@ -5,17 +5,18 @@ import arrow.core.toT
 typealias KleisliOptional<A> = Tuple2<Option<A>, String>
 
 fun main() {
-    val optional20: KleisliOptional<Int> = Option.just(20) toT "просто двадцать"
-    val optional30: KleisliOptional<Int> = Option.fromNullable(null) toT "а тут налл"
+    val optional20: KleisliOptional<Int> = Option.just(20) toT "just twelve"
+    val optional30: KleisliOptional<Int> = Option.fromNullable(null) toT "unfortunately null"
+    val optional40: KleisliOptional<Int> = Option.just(40) toT "forty"
 
-    val composed = optional20 compose optional30
+    val composed = optional20 compose optional30 compose optional40
     val computationResult = composed.a.fold({ 0 }, { it })
     val log = composed.b
-    println("Результат сложения: $computationResult\nЛог: $log")
+    println("Computation result: $computationResult\nLog: $log")
 }
 
 infix fun <A> KleisliOptional<A>.compose(another: KleisliOptional<A>): KleisliOptional<A> {
-    return this.a.and(another.a) toT ("${this.b} а потом вдруг бац и ${another.b}")
+    return this.a.and(another.a) toT ("${this.b} and then ${another.b}")
 }
 
 fun <A> KleisliOptional<A>.identity() = this
